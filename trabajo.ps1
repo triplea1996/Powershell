5f29cd88025e8b698a5133930326e046fbf559f3
triplea1996 <45069198+triplea1996@users.noreply.github.com>
2018-12-09 13:07:44 +0100

    1
    1

---
+++ trabajo.ps1
@@ -0,0 +1,9 @@
+﻿#Encuentra los cinco procesos que usan más memoria.
+ps | sort –p ws | select –last 5
+#Obtener información sobre la fabricación y modelo de un ordenador
+Get-WmiObject -Class Win32_ComputerSystem
+#Obtener serial
+(Get-WmiObject win32_bios).SerialNumber
+
+
+
